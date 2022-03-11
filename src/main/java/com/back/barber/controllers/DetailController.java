a97e9f60151d2d14d6e11212759ae8437050de9b
Oscar915 <52334894+Oscar915@users.noreply.github.com>
2022-03-11 12:46:10 -0500

    Update DetailController.java

--- src/main/java/com/back/barber/controllers/DetailController.java
+++ src/main/java/com/back/barber/controllers/DetailController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200/", methods= {RequestMethod.GET, RequestMethod.POST})
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET, RequestMethod.POST})
 @RequestMapping("/api/detail")
 public class DetailController {
     @Autowired
