beab4659cd0abc0707f22fa46b964791722c19ad
Oscar <oscarballestach@gmail.com>
2022-03-11 12:17:04 -0500

    add origin controls

--- src/main/java/com/back/barber/controllers/DetailController.java
+++ src/main/java/com/back/barber/controllers/DetailController.java
@@ -8,13 +8,16 @@ import com.back.barber.services.DetailService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET, RequestMethod.POST})
 @RequestMapping("/api/detail")
 public class DetailController {
     @Autowired
