b035074a2bdfbacbfee7ccca8842275f0c19d66e
Oscar <oscarballestach@gmail.com>
2022-03-11 11:16:19 -0500

    Quitando properties del test

--- src/test/java/com/back/barber/BarberApplicationTests.java
+++ src/test/java/com/back/barber/BarberApplicationTests.java
@@ -3,7 +3,7 @@ package com.back.barber;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(properties = "spring.main.lazy-initialization=true",classes = {BarberApplicationTests.class})
+@SpringBootTest
 class BarberApplicationTests {
 
 	@Test
